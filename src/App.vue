fca547b8fd5a7db78646f5c27fb332550877e8cb
Lyly <kunlyly1234@gmail.com>
2020-07-23 16:06:59 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Ly />
+</div>
+</template>
+
+<script>
+import Ly from './components/lyly/Ly.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Ly
+  }
+}
+</script>
---
+++ src/components/lyly/Ly.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Hello Bong, From Lyly</h1>
+        <h2>Miss lyly te?</h2>
+        <h3> {{ miss }} </h3>
+    </div>
+</template>
+<script>
+export default {
+    data: () => ({
+        miss : true
+    })
+}
+</script>
+<style scoped>
+
+</style>
